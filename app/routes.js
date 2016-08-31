bef63328a7dd9e3f5ce7ef00fb02adebab7c72ef
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2016-08-31 14:06:14 +0100

    Routes need updating to use POST data and not GET data.

--- app/routes.js
+++ app/routes.js
@@ -2,9 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-
   res.render('index');
-
 });
 
 
@@ -19,7 +17,6 @@ router.get('/examples/template-data', function (req, res) {
 });
 
 // Branching
-
 router.get('/examples/over-18', function (req, res) {
 
   // get the answer from the query string (eg. ?over18=false)
@@ -41,8 +38,19 @@ router.get('/examples/over-18', function (req, res) {
 
 // add your routes here
 // Branching-question iteration
-router.get('/web-form/intention', function (req, res) {
-  var questiontype = req.query.questiontype;
+
+//Clear the session
+router.get('web-form/confirmation', function(req, res){
+    req.session = null;
+    res.render('confirmation');
+});
+
+router.post('/web-form/intention', function(req, res) {
+  var questiontype = req.body.questiontype;
+
+  req.session.questiontype = questiontype;
+
+  if(questiontype)
 
   if (questiontype == "court"){
     res.redirect("1-court");
@@ -58,6 +66,61 @@ router.get('/web-form/intention', function (req, res) {
   } else {
     res.redirect("1-court");
   }
+});
+
+router.post('/web-form/question', function(req, res) {
+  var message = req.body.message;
+
+  if(message === ''){
+    message = '<p>Dear Ministry of Justice,</p>' +
+    '<p>I would like to make a complaint about the condition of Lambeth County Court.</p>' +
+    '<p>Last week I had to attend a hearing at the court, and firstly I found it very hard to find. There was no clear signage from the main roads, and I ended up having to ask several people passing for directions. I ended up arriving slightly late, but was left to wait an extra half an hour anyway. Also the staff at the x ray machine weren\'t very helpful when I asked where I should go.</p>' +
+    '<p>Any way the waiting room I had to sit in was in a terrible state. I don\'t think a cleaner had been around the night before, as it was only 10:45am when I arrived and there was empty Redbull cans and crisp wrappers on the seat next to me. It was also full of people and was incredibly hot. I tried to open the windows and they can\'t be opened!!</p>' +
+    '<p>This court is very old and dusty and looks like it should be renovated. I really think you should invest some money in this, as people who come to court are already stressed and they don\'t need to be left to feel like no body cares. I also think you should have free wifi available so people can access any documents they need to prepare themselves.</p>' +
+    '<p>Please make this court a better place to have to wait, and speak to your staff about being more helpful and friendly, as I don\'t need to deal with rude people when going to court.</p>' +
+    '<p>Kindest regards,</p>' +
+    '<p>Christina Christensen</p>';
+  }
+
+  req.session.message = message;
+
+  res.redirect('2-personal-details');
+});
+
+router.post('/web-form/summary', function(req, res) {
+  var title = req.body.title,
+  firstName = req.body.first_name,
+  lastName = req.body.last_name,
+  email = req.body.email,
+  address = req.body.address;
+
+  //Set some defaults
+  if(firstName ===""){
+    firstName = 'John';
+  }
+
+  if(lastName ===""){
+    lastName = 'Smith';
+  }
+
+  if(email ===""){
+    email = 'John.Smith@example.com'
+  }
+
+  req.session.title= title;
+  req.session.firstName = firstName;
+  req.session.lastName = lastName;
+  req.session.fullName = firstName + ' ' + lastName;
+  req.session.email = email;
+  req.session.address = address;
+
+  res.render('web-form/3-summary', {
+    'email' : req.session.email,
+    'fullname' : req.session.fullName,
+    'question' : req.session.message,
+    'address'  : req.session.address
+  });
+
 });
 
 
